4b4b4053a058c496e7d21d17aa8d19e98e47f934
FurmanovD <Dmitry.Furmanov@gmail.com>
2021-04-14 12:03:20 +0300

    Add Prometheus metrics package

---
+++ prommetrics/counter.go
@@ -0,0 +1,52 @@
+package prommetrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// Counter make an alias for non-decreasing numeric value.
+type Counter prometheus.Counter
+
+// CounterVec interface to work with the Counter labelled.
+type CounterVec interface {
+	WithLabelValues(lvs ...string) prometheus.Counter
+}
+
+// counterMethods
+type counterMethods interface {
+	NewCounter(subsystem, name, help string) Counter
+	NewCounterVec(subsystem, name, help string, labels ...string) CounterVec
+}
+
+// NewCounter creates and wraps out 'current_counter_value_<of>' metrics.
+func (m *metricsHandler) NewCounter(subsystem, name, help string) Counter {
+
+	metric := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Namespace: m.metricsNamespace,
+			Subsystem: subsystem,
+			Name:      name,
+			Help:      help,
+		},
+	)
+	m.addMetricItem(subsystem, name, metric)
+
+	return metric
+}
+
+// NewCounterVec creates and wraps out 'current_counter_value_<of>' metrics with labels set.
+func (m *metricsHandler) NewCounterVec(subsystem, name, help string, labels ...string) CounterVec {
+
+	metric := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: m.metricsNamespace,
+			Subsystem: subsystem,
+			Name:      name,
+			Help:      help,
+		},
+		labels,
+	)
+	m.addMetricItem(subsystem, name, metric)
+
+	return metric
+}
