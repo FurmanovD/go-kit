5cfda76929c79638d5b8391333544db4510209d1
Dmytro Furmanov <39990503+FurmanovD@users.noreply.github.com>
2023-04-02 23:01:12 +0200

    Add some comments (#13)

--- testing/testvalue/sign.go
+++ testing/testvalue/sign.go
@@ -4,6 +4,7 @@ type NumberPosNeg interface {
 	int | int32 | int64 | float32 | float64
 }
 
+// Pos returns abs(n) of the number !=0 or 1 otherwise.
 func Pos[T NumberPosNeg](n T) T {
 	if n == 0 {
 		n = 1
@@ -13,11 +14,13 @@ func Pos[T NumberPosNeg](n T) T {
 	return n
 }
 
+// Neg returns -abs(n) of the number !=0 or -1 otherwise.
 func Neg[T NumberPosNeg](n T) T {
 	// nolint:typecheck
 	return -Pos(n)
 }
 
+// NonPos returns -abs(n) or zero.
 func NonPos[T NumberPosNeg](n T) T {
 	if n > 0 {
 		n *= -1
@@ -25,6 +28,7 @@ func NonPos[T NumberPosNeg](n T) T {
 	return n
 }
 
+// NonNeg returns abs(n) or zero.
 func NonNeg[T NumberPosNeg](n T) T {
 	return -NonPos(n)
 }
